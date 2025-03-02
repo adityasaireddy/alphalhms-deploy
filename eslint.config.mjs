4f069bd9be798118d7b5975d55fedc4c97f6d34b
Aditya Reddy <adityasaireddy112@gmail.com>
2025-03-03 02:06:09 +0530

    thf

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+export default eslintConfig;
+module.exports = {
+  eslint: {
+    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+};
