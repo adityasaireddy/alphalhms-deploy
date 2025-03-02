06a21eb96ee298c402939a71dfd655666bd1cb68
Aditya Reddy <adityasaireddy112@gmail.com>
2025-03-03 02:08:13 +0530

    new

--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-export default eslintConfig;
-module.exports = {
-  eslint: {
-    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
-};
